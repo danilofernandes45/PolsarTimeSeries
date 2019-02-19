f8a8b7a7c910ad05aac235aee772cae0991d74d3
Eduarda Chagas <etcc@ic.ufal.br>
2019-02-19 01:25:32 -0300

    Adding Information Theory

--- Bandt&Pompe/Band&Pompe.R
+++
@@ -1,8 +0,0 @@
-Bandt.Pompe <- function(series, dimension, delay){
-  
-  dyn.load("BandtPompe.so")
-  
-  probability <- .Call("BandtPompe", series, dimension, delay)
-  
-  return (probability)
-}
